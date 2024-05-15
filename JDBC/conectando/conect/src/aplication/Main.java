a41e6185efc46bbf93fbd1667d45e763c61b4b1f
victor.costa213 <132701394+victorcosta213@users.noreply.github.com>
2024-05-15 10:52:50 -0300

    iniciando os estudos de JDBC

---
+++ JDBC/conectando/conect/src/aplication/Main.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+
+    }
+
+}
