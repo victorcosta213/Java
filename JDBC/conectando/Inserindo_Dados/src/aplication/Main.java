b13848a7aa0ba89139f79fcf4031525870a058a3
victor.costa213 <132701394+victorcosta213@users.noreply.github.com>
2024-05-21 15:52:05 -0300

    aprendendo a ler e inserir dados no BD

---
+++ JDBC/conectando/Inserindo_Dados/src/aplication/Main.java
@@ -0,0 +1,57 @@
+package aplication;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try{
+            conn = DB.getConnection();
+            st= conn.prepareStatement(
+                    "INSERT INTO  seller"+
+                            "(Name,Email,BirthDate,BaseSalary, DepartmentId)"+
+                            "VALUES "+
+                            "(?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS
+            );
+
+            st.setString(1,"carl purple");
+            st.setString(2, "carl@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            st.setDouble(4,3000);
+            st.setInt(5,4);
+            int rowAffected = st.executeUpdate();
+
+            if(rowAffected>0){
+                ResultSet rs = st.getGeneratedKeys();
+                while(rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Done! ID = "+id);
+                }
+
+            }else{
+                System.out.println("Nenhuma linha alterada!");
+            }
+            // System.out.println("Done! Rows affected: " + rowAffected);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+
+        }
+
+    }
+
+}
